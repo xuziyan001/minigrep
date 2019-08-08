1f3d77d09e7686271bcf884b359d94e26a1efab3
XuZiyan <xuziyan@p1.com>
2019-08-08 17:05:12 +0800

    fix

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 use std::process;
 
-use minigrep;
-use minigrep::Config;
+use minigrep_shits_always_happen;
+use minigrep_shits_always_happen::Config;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err); // 打印到标准错误流
         process::exit(1);
     });
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = minigrep_shits_always_happen::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
